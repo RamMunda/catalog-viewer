b9f46a6c676e9d2b5833dc2c4b8796d39bf94ab7
Ram Munda <ramkumar2k21@gmail.com>
2021-10-29 17:06:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import preloader from './assets/preloader.gif';
+import close from './assets/close.png';
+import leftArrow from './assets/arrow-left.png';
+import rightArrow from './assets/arrow-right.png';
+
+import {data} from './components/Data';
+
+var setinterval_var;
+var count = 1;
 
 function App() {
+
+  const [currentImage,setcurrentimag] = useState({});
+  const [imagepopup,setimagepopup] = useState(false);
+
+  const onclickhandler = (id,url) =>{
+    setimagepopup(true);
+    setcurrentimag({id:id,url:url});
+    document.querySelector('body').classList.add('body_set_overflow_class');
+  }
+  
+  const imagecheckHandler = (v) =>{
+    if(v==="prev"){
+      if(currentImage.id===1){
+        setcurrentimag({id:data.length,url:data[data.length-1].imgurl});
+        return null;
+      }
+      setcurrentimag({id:currentImage.id-1,url:data[currentImage.id-2].imgurl});
+    }
+    if(v==="next"){
+      if(currentImage.id===data.length){
+        setcurrentimag({id:1,url:data[0].imgurl});
+        return null;
+      }
+      setcurrentimag({id:currentImage.id+1,url:data[currentImage.id].imgurl});
+    }
+  }
+  const resetHandler = () =>{
+    clearInterval(setinterval_var);
+    setcurrentimag({});
+    setimagepopup(false);
+    document.querySelector('body').classList.remove('body_set_overflow_class');
+  }
+
+  const imageSetterfunction = () =>{
+    let currentImage_local_id = currentImage.id;
+
+    if(count===(data.length - currentImage_local_id+1)){
+      count = 1;
+      currentImage_local_id = 0;
+      currentImage.id = 0;
+    }
+    setcurrentimag({id:currentImage_local_id + count,url:data[currentImage_local_id + count-1].imgurl});
+    count++;
+  }
+
+  const checkboxHandler = () =>{
+    var autoslide = document.getElementById('autoslide');
+    if(autoslide.checked === true){
+        setinterval_var =  setInterval(() => {
+          imageSetterfunction();
+      }, 2500);
+    }
+    else{
+      count = 1;
+      clearInterval(setinterval_var);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="image_gallery">
+         <h1>Image Gallery</h1>
+         <div className="image_container">
+            {
+              data ? data.map(d =>(
+                <img key={d.id} src={d.imgurl} alt={d.id + "photo"} onClick={()=>onclickhandler(d.id,d.imgurl)} />
+                )) : <img src={preloader} alt="preloader" />
+            }
+         </div>
+       </div>
+       {
+       imagepopup ?     
+        <div className="slider_wrapper">
+          <div className="sliding_area">
+            <img src={close} alt="close" onClick={resetHandler} />
+            <div className="auto_sliding_checkbox">
+              <form>
+                <input type="checkbox" id="autoslide" name="autoslide" onChange={checkboxHandler} />
+                <label htmlFor="autoslide"> Auto Slide</label>
+              </form>
+            </div>
+            <div className="slider_section">
+            {currentImage ? <img src={currentImage.url} alt="currentimage" /> : null}
+            <div className="left_arrow">
+              <img src={leftArrow} alt="left_arrow" onClick={() =>imagecheckHandler("prev")} />
+            </div>
+            <div className="right_arrow">
+              <img src={rightArrow} alt="right_arrow" onClick={()=>imagecheckHandler("next")} />
+            </div>
+          </div>
+          </div>
+        </div>
+       : null
+       }
+
     </div>
   );
 }
---
+++ src/components/Data.js
@@ -0,0 +1,105 @@
+import pexels_anand_dandekar from '../assets/pexels-anand-dandekar.jpg';
+import pexels_burst from '../assets/pexels-burst.jpg';
+import pexels_cátia_matos from '../assets/pexels-catia-matos.jpg';
+import pexels_ella_olsson from '../assets/pexels-ella-olsson.jpg';
+import pexels_fiona_art from '../assets/pexels-fiona-art.jpg';
+import pexels_flickr from '../assets/pexels-flickr.jpg';
+import pexels_giorgio_de_angelis from '../assets/pexels-giorgio-de-angelis.jpg';
+import pexels_ian_turnell from '../assets/pexels-ian-turnell.jpg';
+import pexels_johannes_plenio from '../assets/pexels-johannes-plenio.jpg';
+import pexels_karolina_grabowska from '../assets/pexels-karolina-grabowska.jpg';
+import pexels_lumn from '../assets/pexels-lumn.jpg';
+import pexels_matheus_bertelli from '../assets/pexels-matheus-bertelli.jpg';
+import pexels_philippe_donn from '../assets/pexels-philippe-donn.jpg';
+import pexels_pixabay_33101 from '../assets/pexels-pixabay-33101.jpg';
+import pexels_pixabay_68507 from '../assets/pexels-pixabay-68507.jpg';
+import pexels_pixabay from '../assets/pexels-pixabay.jpg';
+import pexels_pixabay12 from '../assets/pexels-pixabay12.jpg';
+import pexels_pixabayy from '../assets/pexels-pixabayy.jpg';
+import pexels_simon_berger from '../assets/pexels-simon-berger.jpg';
+import pexels_vlad_alexandru_popa from '../assets/pexels-vlad-alexandru-popa.jpg';
+
+export const data = [
+    {
+        id:1,
+        imgurl: pexels_anand_dandekar
+    },
+    {
+        id:2,
+        imgurl: pexels_burst
+    },
+    {
+        id:3,
+        imgurl: pexels_cátia_matos
+    },
+    {
+        id:4,
+        imgurl: pexels_ella_olsson
+    },
+    {
+        id:5,
+        imgurl: pexels_fiona_art
+    },
+    {
+        id:6,
+        imgurl: pexels_flickr
+    },
+    {
+        id:7,
+        imgurl: pexels_giorgio_de_angelis
+    },
+    {
+        id:8,
+        imgurl: pexels_ian_turnell
+    },
+    {
+        id:9,
+        imgurl: pexels_johannes_plenio
+    },
+    {
+        id:10,
+        imgurl: pexels_karolina_grabowska
+    },
+    {
+        id:11,
+        imgurl: pexels_lumn
+    },
+    {
+        id:12,
+        imgurl: pexels_matheus_bertelli
+    },
+    {
+        id:13,
+        imgurl: pexels_philippe_donn
+    },
+    {
+        id:14,
+        imgurl: pexels_pixabay_33101
+    },
+    {
+        id:15,
+        imgurl: pexels_pixabay_68507
+    },
+    {
+        id:16,
+        imgurl: pexels_pixabay
+    },
+    {
+        id:17,
+        imgurl: pexels_pixabay12
+    },
+    {
+        id:18,
+        imgurl: pexels_pixabayy
+    },
+    {
+        id:19,
+        imgurl: pexels_simon_berger
+    },
+    {
+        id:20,
+        imgurl: pexels_vlad_alexandru_popa
+    },
+    
+
+]
